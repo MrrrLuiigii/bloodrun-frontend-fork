2452d1041f55c3fd58eaff88af00a468eddff2eb
nickyjjanse <i421118@fhict.nl>
2020-01-06 10:52:35 +0100

    slopeeennnnnnn

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -1,78 +1,32 @@
 <template>
-<div>
   <div>
-    <Router-link to="/lobbies">
-        <button
-      class="artXButton"
-    >
-      Play
-    </button>
-    </router-link>
-  </div>
-<div>
-      <Router-link to="/deckbuilder">
-        <button
-      class="artXButton"
-    >
-      DeckBuilder
-    </button>
-        </router-link>
-  </div>
-  <div>
-        <Router-link to="/shop">
-        <button
-      class="artXButton"
-    >
-      Shop
-    </button>
-        </router-link>
-  </div>
-  <div>
-        <Router-link to="/quests">
-        <button
-      class="artXButton"
-    >
-      Quests
-    </button>
-        </router-link>
-  </div>
+    <div>
+      <Router-link to="/lobbies">
+        <button class="artXButton">
+          Play
+        </button>
+      </Router-link>
+    </div>
 
-<div>
-        <Router-link to="/game">
-        <button
-      class="artXButton"
-    >
-      Game
-    </button>
-        </router-link>
-  </div>
-
-  <div>
-          <button
-        class="artButton"
-        @click="logout()"
-      >
+    <div>
+      <button class="artButton" @click="logout()">
         Quit
-      </button >
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-name: "navbar",
-methods: {
-    logout(){
-      if(confirm("Are you sure that you want to logout?")){
+  name: "navbar",
+  methods: {
+    logout() {
+      if (confirm("Are you sure that you want to logout?")) {
         this.$auth.logout();
+      }
     }
   }
-}
-}
-
-
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
