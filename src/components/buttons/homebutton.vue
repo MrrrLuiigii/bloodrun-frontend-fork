e5860dad042f40d7d8ec65d70c2bd2c9ced929d5
nickyjjanse <i421118@fhict.nl>
2020-01-13 10:38:13 +0100

    style homebutton

--- src/components/buttons/homebutton.vue
+++ src/components/buttons/homebutton.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Router-link to="/">
-      <button class="artXButton">
+      <button class="artButton homeButton">
         Home
       </button>
     </Router-link>
@@ -12,4 +12,16 @@ export default {
   name: "homebutton"
 };
 </script>
-<style></style>
+<style>
+.homeButton {
+  position: absolute;
+  left: 10px;
+  bottom: 10px;
+  text-decoration: none;
+  outline: 0;
+}
+
+.homeButton:focus {
+  outline: 0;
+}
+</style>
