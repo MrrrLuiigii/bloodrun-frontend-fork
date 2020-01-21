a6955c40a0df274001fd1b3c1e0dac47d60fd139
nickyjjanse <i421118@fhict.nl>
2020-01-21 22:56:54 +0100

    added readybutton

---
+++ src/components/gamelobby/readyButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    v-if="!playerReady"
+    class="lobbyPlayerReadyButton artButton"
+    @click="ready"
+  >
+    Ready
+  </button>
+  <button v-else class="lobbyPlayerReadyButton artButton" @click="unReady">
+    Unready
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["playerReady"],
+  methods: {
+    async ready() {
+      //call ready stuff to websocket
+    },
+    async unReady() {
+      //call unready stuff to websocket
+    }
+  }
+};
+</script>
+
+<style>
+.lobbyPlayerReadyButton {
+  background-color: #191938;
+
+  position: absolute;
+  bottom: 1vh;
+  left: 4.5vw;
+}
+</style>
